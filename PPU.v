ac883848a983de199469a3795ea5f0288b888399
johan017 <ashley.ortiz14@upr.edu>
2020-10-27 23:53:06 -0400

    Status Register

--- PPU.v
+++ PPU.v
@@ -13,6 +13,8 @@ module control_unit(output ID_B_instr, ID_load_instr, ID_RF_instr, ID_shift_imm,
     integer b_bl = 0; // branch or branch & link
     integer r_sr_off = 0; // register or Scaled register offset
     integer u = 0;
+    integer l = 0;
+    integer condAsserted = 0; // 0 Cond no se da, 1 cond se da
 
     assign ID_shift_imm = s_imm;
     assign ID_RF_instr = rf_instr;
@@ -25,6 +27,13 @@ module control_unit(output ID_B_instr, ID_load_instr, ID_RF_instr, ID_shift_imm,
     begin
         instr = A[27:25];
 
+        if(instr == 3'b101)
+            b_instr = 1;
+        else 
+            b_instr = 0;
+        
+
+
         case(instr)
 
             3'b000: //Data Procesing Shift_by_imm
@@ -32,7 +41,7 @@ module control_unit(output ID_B_instr, ID_load_instr, ID_RF_instr, ID_shift_imm,
                     s_imm = 0; 
                     rf_instr = 1; 
                     l_instr = 0; 
-                    b_instr = 0;
+                    // b_instr = 0;
                     alu_op = A[24:21];
                 end
 
@@ -41,29 +50,49 @@ module control_unit(output ID_B_instr, ID_load_instr, ID_RF_instr, ID_shift_imm,
                     s_imm = 1; 
                     rf_instr = 1; 
                     l_instr = 0; 
-                    b_instr = 0;
+                    // b_instr = 0;
                     alu_op = A[24:21];
                 end
 
             3'b010: //Load/Store Immediate Offset
                 begin
                     u = A[23];
+                    l = A[20];
                     s_imm = 0; 
-                    rf_instr = 1; 
-                    l_instr = A[20]; 
-                    b_instr = 0;
+                    l_instr = l; 
+                    // b_instr = 0;
+
+                    if(l == 0)
+                        rf_instr = 0;
+                    else
+                        rf_instr = 1; 
+                    
 
                     if(u == 1)
                         alu_op = 4'b0100; //suma
                     else
                         alu_op = 4'b0010; //resta
+                    
                             
                 end
 
             3'b011: //Load/Store Register Offset
                 begin
                     u = A[23];
-    
+                    l = A[20];
+
+                    if(u == 1)
+                        alu_op = 4'b0100; //suma
+                    else
+                        alu_op = 4'b0010; //resta
+                    
+
+                    if(l == 0)
+                        rf_instr = 0;
+                    else
+                        rf_instr = 1; 
+                    
+
                     if(A[11:4] == 8'b00000000)
                         r_sr_off = 0;
                     else
@@ -71,27 +100,18 @@ module control_unit(output ID_B_instr, ID_load_instr, ID_RF_instr, ID_shift_imm,
                     
 
                     
-                    if(r_sr_off == 0)begin
+                    if(r_sr_off == 0)begin //register_offset
                         s_imm = 0; 
-                        rf_instr = 1; 
-                        l_instr = A[20]; 
-                        b_instr = 0;
+                        l_instr = l; 
+                        // b_instr = 0;
 
-                        if(u == 1)
-                            alu_op = 4'b0100; //suma
-                        else
-                            alu_op = 4'b0010; //resta
-
-                    end else begin
+                
+                    end else begin //scaled_reg_offset
                         s_imm = 0; 
-                        rf_instr = 1; 
-                        l_instr = A[20]; 
-                        b_instr = 0;
+                        l_instr = l; 
+                        // b_instr = 0;
 
-                        if(u == 1)
-                            alu_op = 4'b0100; //suma
-                        else
-                            alu_op = 4'b0010; //resta
+                       
                             
                     end
                   
@@ -100,21 +120,22 @@ module control_unit(output ID_B_instr, ID_load_instr, ID_RF_instr, ID_shift_imm,
             3'b101: //branches
                 begin
                     b_bl = A[24];
-                    
+                  
                     case(b_bl)
                         1'b0://branch
                             begin
                                 s_imm = 0; 
                                 rf_instr = 0; 
                                 l_instr = 0; 
-                                b_instr = 1;
+                                // b_instr = 1;
                             end
                         1'b1://branch & link
                             begin
                                 s_imm = 0; 
                                 rf_instr = 1; 
                                 l_instr = 0; 
-                                b_instr = 1;
+                                // b_instr = 1;
+                                alu_op = 4'b0100; //suma
                             end
                     endcase
                    
@@ -125,44 +146,63 @@ module control_unit(output ID_B_instr, ID_load_instr, ID_RF_instr, ID_shift_imm,
     end
 endmodule
 
+//Status Register
+module Status_register(input [3:0] cc_in, input S, output reg [3:0] cc_out, input clk);
+    
+    always@(clk)
+
+    
+    begin
+
+        if(clk == 0)
+            cc_out = 4'b0;
+        else
+            if(S == 1)
+                cc_out = cc_in; 
+            else 
+                cc_out = 4'b0; //si no lo modifica va un register con el valor anterior
+    end
+
+endmodule
+
 
 //IF/ID PIPELINE REGISTER
 module IF_ID_pipeline_register(input clk);
 
-always@(posedge clk)
-    begin
+    always@(posedge clk)
+        begin
         
-    end
+        end
 endmodule
 
 
 //ID/EX PIPELINE REGISTER
 module ID_EX_pipeline_register(input clk);
 
-always@(posedge clk)
-    begin
-        
-    end
+    always@(posedge clk)
+        begin
+            
+        end
 endmodule
 
 
 //EX/MEM PIPELINE REGISTER
 module EX_MEM_pipeline_register(input clk);
 
-always@(posedge clk)
-    begin
-        
-    end
+    always@(posedge clk)
+        begin
+            
+        end
 endmodule
 
 
 //MEM/WB PIPELINE REGISTER
 module MEM_WB_pipeline_register(input clk);
 
-always@(posedge clk)
-    begin
-        
-    end
+    always@(posedge clk)
+        begin
+            
+        end
 endmodule
 
 
--- PPU_tb.v
+++ PPU_tb.v
@@ -1,45 +1,61 @@
 `include "PPU.v"
 module PPU_tb;
 
-/*-------------------------------------- PRECHARGE INSTRUCTION RAM --------------------------------------*/
+    /*-------------------------------------- PRECHARGE INSTRUCTION RAM --------------------------------------*/
 
-integer file, fw, code, i; reg [31:0] data;
-reg Enable;
-reg [31:0] Address; wire [31:0] DataOut;
+    integer file, fw, code, i; reg [31:0] data;
+    reg Enable;
+    reg [31:0] Address; wire [31:0] DataOut;
 
-inst_ram256x8 ram1 (DataOut, Enable, Address );
+    inst_ram256x8 ram1 (DataOut, Enable, Address );
 
-initial
-    begin
-    file = $fopen("ramintr.txt","rb");
-    Address = 32'b00000000000000000000000000000000;
-        while (!$feof(file)) begin //while not the end of file
-        code = $fscanf(file, "%b", data);
-        ram1.Mem[Address] = data;
-        Address = Address + 1;
+    initial
+        begin
+        file = $fopen("ramintr.txt","rb");
+        Address = 32'b00000000000000000000000000000000;
+            while (!$feof(file)) begin //while not the end of file
+            code = $fscanf(file, "%b", data);
+            ram1.Mem[Address] = data;
+            Address = Address + 1;
+        end
+
+    $fclose(file);  
     end
 
-$fclose(file);  
-end
-
-initial begin
-    fw = $fopen("ramintr_content.txt", "w");
-    Enable = 1'b0; 
-    Address = #1 32'b00000000000000000000000000000000; //make sure adress is in 0 after precharge
-    repeat (9) begin
-    #5 Enable = 1'b1;
-    #5 Enable = 1'b0;
-    Address = Address + 4;
-end
-$finish;
-end
-always @ (posedge Enable)
-    begin
-    #1;   
-    $fdisplay(fw,"Data en %d = %b %d", Address, DataOut, $time);
-end
+    initial begin
+        fw = $fopen("ramintr_content.txt", "w");
+        Enable = 1'b0; 
+        Address = #1 32'b00000000000000000000000000000000; //make sure adress is in 0 after precharge
+        repeat (9) begin
+        #5 Enable = 1'b1;
+        #5 Enable = 1'b0;
+        Address = Address + 4;
+    end
+    $finish;
+    end
+    always @ (posedge Enable)
+        begin
+        #1;   
+        $fdisplay(fw,"Data en %d = %b %d", Address, DataOut, $time);
+    end
 
 
+    /*-------------------------------------- STATUS REGISTER --------------------------------------*/
+    reg clk = 1;
+    reg [3:0] cc_in;
+    wire [3:0] cc_out;
+    reg S = 0;
+    
+    initial begin
+        cc_in = 4'b1111;
+        #20;
+        $display("CC in = %d", stat.cc_in);
+        $display("CC out = %d", stat.cc_out);
+        $display("S = %d", stat.S);
+        $display("Clk = %d", stat.clk);
 
+    end
+
+    Status_register stat(cc_in, S, cc_out, clk);
 
 endmodule
