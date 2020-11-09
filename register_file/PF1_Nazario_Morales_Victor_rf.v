a6d274df489bc17ddab515e8eeab59befc461fe6
Victor Nazario <victor.nazario@upr.edu>
2020-11-09 14:51:04 -0400

    Cleanup RF

--- register_file/PF1_Nazario_Morales_Victor_rf.v
+++ register_file/PF1_Nazario_Morales_Victor_rf.v
@@ -160,9 +160,7 @@ module PCregister(Q, PW, PCin, RFLd, CLK, RST);
 
     //Used as a reset mechanism, when RST is equals to one, the R15 will get a 0 value
     always @ (posedge RST)
-    begin
         Q <= 0;
-    end
 
     always @ (posedge CLK)
     begin
@@ -174,166 +172,167 @@ module PCregister(Q, PW, PCin, RFLd, CLK, RST);
 
 endmodule
 
-module tester;
-    //Variable for loop
-    integer index;
-    //Inputs
-    reg CLK, RFLd, PCLd, RST;
-    reg [3:0] SA, SB, SD, SPCout, C;
-    reg [31:0] PW, PCin;
-
-    //Outputs
-    wire [31:0] PA, PB, PD, PCout;
-
-    initial RST = 1'b1;
-
-    //Clock Signal
-    always begin
-        #5;
-        PCin = PCin + 4;
-        CLK = ~CLK;
-    end
-
-    //PCLoad signal
+//module tester;
+//    //Variable for loop
+//    integer index;
+//    //Inputs
+//    reg CLK, RFLd, PCLd, RST;
+//    reg [3:0] SA, SB, SD, SPCout, C;
+//    reg [31:0] PW, PCin;
+//
+//    //Outputs
+//    wire [31:0] PA, PB, PD, PCout;
+//
+//    initial RST = 1'b1;
+//
+//    //Clock Signal
 //    always begin
-//        #2;
-//        PCLd = ~PCLd;
+//        #5;
+//        PCin = PCin + 4;
+//        CLK = ~CLK;
 //    end
-
-
-//    Will print values for each tick of the clock. All 32bit values displayed in decimal
-//    without trailing zeroes, binary otherwise.
-     always @ (CLK)
-     begin
-         //$display("PC:%0d | PW:%0d | SA:%b | SB:%b | SD:%b | PA:%0d | PB:%0d | PD:%0d | C:%b | PCLd:%b | PCout: %3d", PCin, PW, SA, SB, SD, PA, PB, PD, C, PCLd, PCout);
-     end
-
-    register_file test (.PA(PA), .PB(PB), .PD(PD), .PW(PW), .PCin(PCin), .PCout(PCout), .C(C), .SA(SA), .SB(SB), .SD(SD), .RFLd(RFLd), .PCLd(PCLd), .CLK(CLK), .RST(RST));
-    initial begin
-        //Initial values
-        PW = 32'b0;
-        C = 4'b0000;
-        SA = 4'b0000;
-        SB = 4'b0000;
-        SD = 4'b0000;
-        RFLd = 1'b0;
-        PCLd = 1'b0;
-        CLK = 1'b1;
-        PCin = 32'b0;
-        RST = 1'b0;
-
-        //Enable load in each register (Ld = 1)
-        #10;
-        RFLd = 1'b1;
-        PCLd = 1'b1;
-
-        //Writing a unique word of each register using Port C(PC)//
-
-        //Register 0
-        #10;
-        C = 4'b0000;
-        PW = 32'd0;
-        SA = 4'b0000;
-        SB = 4'b0000;
-        SD = 4'b0000;
-
-
-        //Register 1
-        #10;
-        C = 4'b0001;
-        PW = 32'd3;
-        SA = 4'b0001;
-        SB = 4'b0001;
-        SD = 4'b0001;
-
-        //Register 2
-        #10;
-        C = 4'b0010;
-        PW = 32'd7;
-        SA = 4'b0010;
-        SB = 4'b0010;
-        SD = 4'b0010;
-
-        //Register 3
-        #10;
-        C = 4'b0011;
-        PW = 32'd90;
-        SA = 4'b0011;
-        SB = 4'b0011;
-        SD = 4'b0011;
-
-        //Register 4
-        #10;
-        C = 4'b0100;
-        PW = 32'd17;
-
-        //Register 5
-        #10;
-        C = 4'b0101;
-        PW = 32'd73;
-
-        //Register 6
-        #10;
-        C = 4'b0110;
-        PW = 32'd6;
-
-        //Register 7
-        #10;
-        C = 4'b0111;
-        PW = 32'd50;
-
-        //Register 8
-        #10;
-        C = 4'b1000;
-        PW = 32'd45;
-
-        //Register 9
-        #10;
-        C = 4'b1001;
-        PW = 32'd18;
-
-        //Register 10
-        #10;
-        C = 4'b1010;
-        PW = 32'd9;
-
-        //Register 11
-        #10;
-        C = 4'b1011;
-        PW = 32'd6;
-
-        //Register 12
-        #10;
-        C = 4'b1100;
-        PW = 32'd24;
-
-        //Register 13
-        #10;
-        C = 4'b1101;
-        PW = 32'd21;
-
-        //Register 14
-        #10;
-        C = 4'b1110;
-        PW = 32'd83;
-
-        //Register 15
-        #10;
-        C = 4'b1111;
-        PW = 32'd35;
-
-
-
-        //This changes the word in R10 and reads said word via Port A(PA).
-        #10;
-        C = 4'b1010;
-        PW = 32'd16;
-        #10
-        SA = 4'b1010;
-        //Showing output through PA, after changing the word in Register 10
-        //$monitor ("Output of Register ", SA, " (using PA) (After Change): PA: %0d",PA);
-        // $monitor("Output of Register 15: %0d", PD, " with PCout:%0d, PCLd was:%d", PCout, PCLd, " Output of Register ", SA, " (using PA) (After Change): PA: %0d" ,PA);
-    $finish;
-    end
-
-endmodule
+//
+//    //PCLoad signal
+////    always begin
+////        #2;
+////        PCLd = ~PCLd;
+////    end
+//
+//
+////    Will print values for each tick of the clock. All 32bit values displayed in decimal
+////    without trailing zeroes, binary otherwise.
+//     always @ (CLK)
+//     begin
+//         $display("PC:%3d | PW:%3d | SA:%b | SB:%b | SD:%b | PA:%3d | PB:%3d | PD:%3d | C:%b | PCLd:%b | PCout: %3d", PCin, PW, SA, SB, SD, PA, PB, PD, C, PCLd, PCout);
+//     end
+//
+//    register_file test (.PA(PA), .PB(PB), .PD(PD), .PW(PW), .PCin(PCin), .PCout(PCout), .C(C), .SA(SA), .SB(SB), .SD(SD), .RFLd(RFLd), .PCLd(PCLd), .CLK(CLK), .RST(RST));
+//    initial begin
+//        //Initial values
+//        PW = 32'b0;
+//        C = 4'b0000;
+//        SA = 4'b0000;
+//        SB = 4'b0000;
+//        SD = 4'b0000;
+//        RFLd = 1'b0;
+//        PCLd = 1'b0;
+//        CLK = 1'b1;
+//        PCin = 32'b0;
+//        RST = 1'b0;
+//
+//        //Enable load in each register (Ld = 1)
+//        #10;
+//        RFLd = 1'b1;
+//        PCLd = 1'b1;
+//
+//        //Writing a unique word of each register using Port C(PC)//
+//
+//        //Register 0
+//        #10;
+//        C = 4'b0000;
+//        PW = 32'd0;
+//        SA = 4'b0000;
+//        SB = 4'b0000;
+//        SD = 4'b0000;
+//
+//
+//        //Register 1
+//        #10;
+//        C = 4'b0001;
+//        PW = 32'd3;
+//        SA = 4'b0001;
+//        SB = 4'b0001;
+//        SD = 4'b0001;
+//
+//        //Register 2
+//        #10;
+//        C = 4'b0010;
+//        PW = 32'd7;
+//        SA = 4'b0010;
+//        SB = 4'b0010;
+//        SD = 4'b0010;
+//
+//        //Register 3
+//        #10;
+//        C = 4'b0011;
+//        PW = 32'd90;
+//        SA = 4'b0011;
+//        SB = 4'b0011;
+//        SD = 4'b0011;
+//
+//        //Register 4
+//        #10;
+//        C = 4'b0100;
+//        PW = 32'd17;
+//
+//        //Register 5
+//        #10;
+//        C = 4'b0101;
+//        PW = 32'd73;
+//
+//        //Register 6
+//        #10;
+//        C = 4'b0110;
+//        PW = 32'd6;
+//
+//        //Register 7
+//        #10;
+//        C = 4'b0111;
+//        PW = 32'd50;
+//
+//        //Register 8
+//        #10;
+//        C = 4'b1000;
+//        PW = 32'd45;
+//
+//        //Register 9
+//        #10;
+//        C = 4'b1001;
+//        PW = 32'd18;
+//        RST = 1'b1;
+//
+//        //Register 10
+//        #10;
+//        C = 4'b1010;
+//        PW = 32'd9;
+//
+//        //Register 11
+//        #10;
+//        C = 4'b1011;
+//        PW = 32'd6;
+//
+//        //Register 12
+//        #10;
+//        C = 4'b1100;
+//        PW = 32'd24;
+//
+//        //Register 13
+//        #10;
+//        C = 4'b1101;
+//        PW = 32'd21;
+//
+//        //Register 14
+//        #10;
+//        C = 4'b1110;
+//        PW = 32'd83;
+//
+//        //Register 15
+//        #10;
+//        C = 4'b1111;
+//        PW = 32'd35;
+//
+//
+//
+//        //This changes the word in R10 and reads said word via Port A(PA).
+//        #10;
+//        C = 4'b1010;
+//        PW = 32'd16;
+//        #10
+//        SA = 4'b1010;
+//        //Showing output through PA, after changing the word in Register 10
+//        //$monitor ("Output of Register ", SA, " (using PA) (After Change): PA: %0d",PA);
+//        // $monitor("Output of Register 15: %0d", PD, " with PCout:%0d, PCLd was:%d", PCout, PCLd, " Output of Register ", SA, " (using PA) (After Change): PA: %0d" ,PA);
+//    $finish;
+//    end
+//
+//endmodule
