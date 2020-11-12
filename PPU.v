ec0359c75dbcdbe4a6e3bc04c30ab385398f8bdf
Valerie <valerie.otero2@upr.edu>
2020-11-11 20:38:49 -0400

    updated clk

--- PPU.v
+++ PPU.v
@@ -74,6 +74,7 @@ module main(); //input clk, input Reset);
     wire[7:0] EX_addresing_modes, ID_addresing_modes;
     wire [6:0] ID_CU, C_U_out, NOP_S;// = 0010001;
 
+
   /*-------------------------------------- PRECHARGE INSTRUCTION RAM --------------------------------------*/
 
     integer file, fw, code, i; reg [31:0] data;   
@@ -94,33 +95,35 @@ module main(); //input clk, input Reset);
     $fclose(file);  
     Address = #1 32'b00000000000000000000000000000000; //make sure adress starts back in 0 after precharge
     end
+   
+
+    /*--------------------------------------  Toggle Clock  --------------------------------------*/
+
+    initial #22 $finish; //finish simulation on tick 22
 
     initial begin
-        
-        $display("\n\n                    ------------------ID State-------------------            ------------------EX State------------------           --------MEM State------          -------WB State-------       -------Instruction-------");
-        $display("         PC      B_instr | shift_imm |   alu  | load | R F | mem_r_w           shift_imm | alu  | load | R F | mem_r_w                load | R F | mem_r_w                load | R F              ");
-    end
+
+        clk = 1'b0; //before tick starts, clk=0
+
+        repeat(22) #1 clk = ~clk; end  //enough repeats to read all instructions 
 
     /*--------------------------------------  Toggle Reset  --------------------------------------*/
 
-    initial begin
-        clk = 1'b0;
-        Reset = 1'b1;
-        #30 $finish;
-    end
-      
-    /*--------------------------------------  Toggle Clock  --------------------------------------*/
+    initial fork       
 
-    always begin
-        $display(Reset);
-        clk = ~clk; 
-        
-        #2 Reset = 1'b0;
-       
-        $display("%d           %b   |     %b     |  %b  |  %b   |  %b  |  %b                      %b  | %b |   %b  |  %b  | %b                         %b |  %b  | %b                         %b |  %b             %b",  PCO, ID_B_instr, C_U_out[6], C_U_out[5:2], C_U_out[1], C_U_out[0], ID_mem_read_write, EX_Shift_imm, EX_ALU_OP, EX_load_instr, EX_RF_Enable,EX_mem_read_write, MEM_load_instr, MEM_RF_Enable, MEM_mem_read_write, WB_load_instr, WB_RF_Enable, DO_CU);
+        Reset = 1'b1; //before tick starts, reset=0
+
+        #2 Reset = 1'b0; //after two ticks, change value to 0                    
       
-    end 
+    join 
   
+    initial begin
+          
+        $display("\n\n                    ------------------ID State-------------------            ------------------EX State------------------           --------MEM State------          -------WB State-------       -------Instruction-------");
+        $display("         PC      B_instr | shift_imm |   alu  | load | R F | mem_r_w           shift_imm | alu  | load | R F | mem_r_w                load | R F | mem_r_w                load | R F              ");
+        $monitor("%d           %b   |     %b     |  %b  |  %b   |  %b  |  %b                      %b  | %b |   %b  |  %b  | %b                         %b |  %b  | %b                         %b |  %b             %b",  PCO, ID_B_instr, C_U_out[6], C_U_out[5:2], C_U_out[1], C_U_out[0], ID_mem_read_write, EX_Shift_imm, EX_ALU_OP, EX_load_instr, EX_RF_Enable,EX_mem_read_write, MEM_load_instr, MEM_RF_Enable, MEM_mem_read_write, WB_load_instr, WB_RF_Enable, DO_CU);
+
+    end
     
         //IF Stage
         //para escoger entre TA & PC+4
