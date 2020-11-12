e7ff2715e0cf4177d6471d567f5c3f2b56ef7e33
Victor Nazario <victor.nazario@upr.edu>
2020-11-12 14:03:43 -0400

    Remove PCLd signal

--- register_file/PF1_Nazario_Morales_Victor_rf.v
+++ register_file/PF1_Nazario_Morales_Victor_rf.v
@@ -3,7 +3,7 @@
 //Description: Defines all the needed components (here modules) for the correct functionality of
 //a register file according to PF1 specifications.
 
-module register_file(PA, PB, PD, PW, PCin, PCout, C, SA, SB, SD, RFLd, PCLd, HZPCld, CLK, RST);
+module register_file(PA, PB, PD, PW, PCin, PCout, C, SA, SB, SD, RFLd, HZPCld, CLK, RST);
     //Outputs
     output [31:0] PA, PB, PD, PCout;
     output [31:0] MO; //output of the 2x1 multiplexer
@@ -11,7 +11,7 @@ module register_file(PA, PB, PD, PW, PCin, PCout, C, SA, SB, SD, RFLd, PCLd, HZP
     //Inputs
     input [31:0] PW, PCin;
     input [3:0] SA, SB, SD, C;
-    input RFLd, PCLd, CLK, RST, HZPCld;
+    input RFLd, CLK, RST, HZPCld;
 
     wire [31:0] Q0, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10, Q11, Q12, Q13, Q14, Q15;
     wire [15:0] E;
@@ -25,12 +25,12 @@ module register_file(PA, PB, PD, PW, PCin, PCout, C, SA, SB, SD, RFLd, PCLd, HZP
     multiplexer muxD (PD, Q0, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10, Q11, Q12, Q13, Q14, Q15, SD);
 
 
-    loadDecoder r15decoder(PCLd, E[15], R15MO);
+    //loadDecoder r15decoder(E[15], R15MO);
 
     //Added this 2x1 multi to handle R15 input variations
     //Here PC is equivalent to PW in the diagram and PCin
     //is the equivalent to the PC (which gets increaed by 4)
-    twoToOneMultiplexer r15mux (PW, PCin, PCLd, MO);
+    twoToOneMultiplexer r15mux (PW, PCin, E[15], MO);
 
 
     //16 Registers
@@ -117,45 +117,45 @@ module multiplexer(P, I0, I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, I11, I12, I13
 endmodule
 
 //This defines the multiplexer used to change inputs to r15 conditionally
-module twoToOneMultiplexer(PW, PC, R15MO, MO);
+module twoToOneMultiplexer(PW, PC, PWLd, MO);
     //Output
     output reg [31:0] MO;
     //Input
     input[31:0] PW, PC;
-    input R15MO;
+    input PWLd;
 
     //Whenever a change is produced in the signals, change the output
     //according with the stablished logic.
-    always @(PW, PC, R15MO)
+    always @(PW, PC, PWLd)
     begin
-        if (R15MO)
-            MO <= PC;
-        else
+        if (PWLd)
             MO <= PW;
-    end
-endmodule
-
-module loadDecoder(PCLd, RFLd, R15MO);
-//When the binary decoder assigns a value of one to E[15] that means R15 has RFLd = 1,
-// thus we write PW instead of PCin. So R15 is going to be 1 which in terms means PW will be loaded.
-//Otherwise we set it to 0 and PCin is loaded into the register.
-output reg[1:0] R15MO;
-input PCLd, RFLd;
-
-//What happens when both = 1?
-//Does this means that the following is accomplished?
-
-//El PC tendrá una señal de “load enable” que cuando esté activa permitirá que el valor externo se cargue en el PC cuando
-//ocurra el “rising edge” del reloj del sistema, excepto cuando el puerto de entrada trate de escribir
-//el mismo, lo cual tiene prioridad.
-always @ (PCLd, RFLd)
-    begin
-        if(RFLd)
-          R15MO <= 1'b1;
         else
-          R15MO <= 1'b0;
+            MO <= PC;
     end
 endmodule
+
+//module loadDecoder(RFLd, R15MO);
+////When the binary decoder assigns a value of one to E[15] that means R15 has RFLd = 1,
+//// thus we write PW instead of PCin. So R15 is going to be 1 which in terms means PW will be loaded.
+////Otherwise we set it to 0 and PCin is loaded into the register.
+//output reg[1:0] R15MO;
+//input RFLd;
+//
+////What happens when both = 1?
+////Does this means that the following is accomplished?
+//
+////El PC tendrá una señal de “load enable” que cuando esté activa permitirá que el valor externo se cargue en el PC cuando
+////ocurra el “rising edge” del reloj del sistema, excepto cuando el puerto de entrada trate de escribir
+////el mismo, lo cual tiene prioridad.
+//always @ (RFLd)
+//    begin
+//        if(RFLd)
+//          R15MO <= 1'b1;
+//        else
+//          R15MO <= 1'b0;
+//    end
+//endmodule
 
 
 module register(Q, PW, RFLd, CLK);
@@ -194,181 +194,171 @@ module PCregister(Q, MOin, HZPCld, CLK, RST);
     end
 endmodule
 
-//module tester;
-//    //Variable for loop
-//    integer index;
-//    //Inputs
-//    reg CLK, RFLd, PCLd, RST, HZPCLd;
-//    reg [3:0] SA, SB, SD, SPCout, C;
-//    reg [31:0] PW, PCin;
-//
-//    //Outputs
-//    wire [31:0] PA, PB, PD, PCout;
-//
-//    initial RST = 1'b1;
-//
-//    initial HZPCLd = 1'b1;
-//
-//
-//
-//    //Clock Signal
-//    always begin
-//        #5;
-//        PCin = PCin + 4;
-//        CLK = ~CLK;
-//    end
-//
-//    //PCLoad signal
-////    always begin
-////        #2;
-////        PCLd = ~PCLd;
-////    end
-//
-//
-////    Will print values for each tick of the clock. All 32bit values displayed in decimal
-////    without trailing zeroes, binary otherwise.
-//     always @ (CLK)
-//     begin
-//         $display("PC:%3d | PW:%3d | SA:%b | SB:%b | SD:%b | PA:%3d | PB:%3d | PD:%3d | C:%b | PCLd:%b | PCout: %3d", PCin, PW, SA, SB, SD, PA, PB, PD, C, PCLd, PCout);
-//         //$display("PC:%3d | PCout: %3d", PCin, PCout);
-//     end
-//
-//    register_file test (.PA(PA), .PB(PB), .PD(PD), .PW(PW), .PCin(PCin), .PCout(PCout), .C(C), .SA(SA), .SB(SB), .SD(SD), .RFLd(RFLd), .PCLd(PCLd), .HZPCld(HZPCLd), .CLK(CLK), .RST(RST));
-//    initial begin
-//    //$monitor("PC:%3d | PCout: %3d | PCLd:%b | RFLd:%3d | HZPCLd :%b", PCin, PCout, PCLd, RFLd, HZPCLd);
-//        //Initial values
-//        PW = 32'b0;
-//        C = 4'b0000;
-//        SA = 4'b0000;
-//        SB = 4'b0000;
-//        SD = 4'b0000;
-//        RFLd = 1'b0;
-//        PCLd = 1'b0;
-//        CLK = 1'b1;
-//        PCin = 32'b0;
-//        RST = 1'b0;
-//
-//        //Enable load in each register (Ld = 1)
-//        #10;
-//        RFLd = 1'b1;
-//        PCLd = 1'b1;  //Tells if we write PC or PW
-//
-//        //Writing a unique word of each register using Port C(PC)//
-//
-//        //Register 0
-//        #10;
-//        C = 4'b0000;
-//        PW = 32'd0;
-//        SA = 4'b0000;
-//        SB = 4'b0000;
-//        SD = 4'b0000;
-//
-//
-//        //Register 1
-//        #10;
-//        C = 4'b0001;
-//        PW = 32'd3;
-//        SA = 4'b0001;
-//        SB = 4'b0001;
-//        SD = 4'b0001;
-//
-//        //Register 2
-//        #10;
-//        C = 4'b0010;
-//        PW = 32'd7;
-//        SA = 4'b0010;
-//        SB = 4'b0010;
-//        SD = 4'b0010;
-//
-//        //Register 3
-//        #10;
-//        C = 4'b0011;
-//        PW = 32'd90;
-//        SA = 4'b0011;
-//        SB = 4'b0011;
-//        SD = 4'b0011;
-//
-//        //Register 4
-//        #10;
-//        C = 4'b0100;
-//        PW = 32'd17;
-//
-//        //Register 5
-//        #10;
-//        C = 4'b0101;
-//        PW = 32'd73;
-//
-//        //Register 6
-//        #10;
-//        C = 4'b0110;
-//        PW = 32'd6;
-//
-//        //Register 7
-//        #10;
-//        C = 4'b0111;
-//        PW = 32'd50;
-//
-//        //Register 8
-//        #10;
-//        C = 4'b1000;
-//        PW = 32'd45;
-//
-//        //Register 9
-//        #10;
-//        C = 4'b1001;
-//        PW = 32'd18;
-//
-//        //Register 10
-//        #10;
-//        C = 4'b1010;
-//        PW = 32'd9;
-//        //RST = 1'b1;    //Can be used to cause a RST
-//        //HZPCLd = 1'b0; //Can be used to cause PC to not increment
-//
-//
-//        //Register 11
-//        #10;
-//        C = 4'b1011;
-//        PW = 32'd6;
-//
-//        //Register 12
-//        #10;
-//        C = 4'b1100;
-//        PW = 32'd24;
-//
-//        //Register 13
-//        #10;
-//        C = 4'b1101;
-//        PW = 32'd21;
-//
-//
-//        //Register 14
-//        #10;
-//        C = 4'b1110;
-//        PW = 32'd83;
-//
-//
-//        //Register 15
-//        #10;
-//        C = 4'b1111;
-//        PW = 32'd35;
-//
-//        //Won't charge PCin, it will charge PW instead given these two signals bellow.
-//        PCLd = 1'b0;
-//        RFLd = 1'b1;
-//
-//
-//
-//        //This changes the word in R10 and reads said word via Port A(PA).
-//        #10;
-//        C = 4'b1010;
-//        PW = 32'd16;
-//        #10
-//        SA = 4'b1010;
-//        //Showing output through PA, after changing the word in Register 10
-//        //$monitor ("Output of Register ", SA, " (using PA) (After Change): PA: %0d",PA);
-//        // $monitor("Output of Register 15: %0d", PD, " with PCout:%0d, PCLd was:%d", PCout, PCLd, " Output of Register ", SA, " (using PA) (After Change): PA: %0d" ,PA);
-//    $finish;
-//    end
-//
-//endmodule
+module tester;
+    //Variable for loop
+    integer index;
+    //Inputs
+    reg CLK, RFLd, RST, HZPCLd;
+    reg [3:0] SA, SB, SD, SPCout, C;
+    reg [31:0] PW, PCin;
+
+    //Outputs
+    wire [31:0] PA, PB, PD, PCout;
+
+    initial RST = 1'b1;
+
+    initial HZPCLd = 1'b1;
+
+
+
+    //Clock Signal
+    always begin
+        #5;
+        PCin = PCin + 4;
+        CLK = ~CLK;
+    end
+
+
+//    Will print values for each tick of the clock. All 32bit values displayed in decimal
+//    without trailing zeroes, binary otherwise.
+     always @ (CLK)
+     begin
+         $display("PC:%3d | PW:%3d | SA:%b | SB:%b | SD:%b | PA:%3d | PB:%3d | PD:%3d | C:%b | PCout: %3d", PCin, PW, SA, SB, SD, PA, PB, PD, C, PCout);
+         //$display("PC:%3d | PCout: %3d", PCin, PCout);
+     end
+
+    register_file test (.PA(PA), .PB(PB), .PD(PD), .PW(PW), .PCin(PCin), .PCout(PCout), .C(C), .SA(SA), .SB(SB), .SD(SD), .RFLd(RFLd), .HZPCld(HZPCLd), .CLK(CLK), .RST(RST));
+    initial begin
+    //$monitor("PC:%3d | PCout: %3d | PCLd:%b | RFLd:%3d | HZPCLd :%b", PCin, PCout, PCLd, RFLd, HZPCLd);
+        //Initial values
+        PW = 32'b0;
+        C = 4'b0000;
+        SA = 4'b0000;
+        SB = 4'b0000;
+        SD = 4'b0000;
+        RFLd = 1'b0;
+        CLK = 1'b1;
+        PCin = 32'b0;
+        RST = 1'b0;
+
+        //Enable load in each register (Ld = 1)
+        #10;
+        RFLd = 1'b1;
+
+        //Writing a unique word of each register using Port C(PC)//
+
+        //Register 0
+        #10;
+        C = 4'b0000;
+        PW = 32'd0;
+        SA = 4'b0000;
+        SB = 4'b0000;
+        SD = 4'b0000;
+
+
+        //Register 1
+        #10;
+        C = 4'b0001;
+        PW = 32'd3;
+        SA = 4'b0001;
+        SB = 4'b0001;
+        SD = 4'b0001;
+
+        //Register 2
+        #10;
+        C = 4'b0010;
+        PW = 32'd7;
+        SA = 4'b0010;
+        SB = 4'b0010;
+        SD = 4'b0010;
+
+        //Register 3
+        #10;
+        C = 4'b0011;
+        PW = 32'd90;
+        SA = 4'b0011;
+        SB = 4'b0011;
+        SD = 4'b0011;
+
+        //Register 4
+        #10;
+        C = 4'b0100;
+        PW = 32'd17;
+
+        //Register 5
+        #10;
+        C = 4'b0101;
+        PW = 32'd73;
+
+        //Register 6
+        #10;
+        C = 4'b0110;
+        PW = 32'd6;
+
+        //Register 7
+        #10;
+        C = 4'b0111;
+        PW = 32'd50;
+
+        //Register 8
+        #10;
+        C = 4'b1000;
+        PW = 32'd45;
+
+        //Register 9
+        #10;
+        C = 4'b1001;
+        PW = 32'd18;
+
+        //Register 10
+        #10;
+        C = 4'b1010;
+        PW = 32'd9;
+        //RST = 1'b1;    //Can be used to cause a RST
+        //HZPCLd = 1'b0; //Can be used to cause PC to not increment
+
+
+        //Register 11
+        #10;
+        C = 4'b1011;
+        PW = 32'd6;
+
+        //Register 12
+        #10;
+        C = 4'b1100;
+        PW = 32'd24;
+
+        //Register 13
+        #10;
+        C = 4'b1101;
+        PW = 32'd21;
+
+
+        //Register 14
+        #10;
+        C = 4'b1110;
+        PW = 32'd83;
+
+
+        //Register 15
+        #10;
+        C = 4'b1111;
+        PW = 32'd35;
+
+        //Won't charge PCin, it will charge PW instead given the signal bellow.
+        RFLd = 1'b1;
+
+
+
+        //This changes the word in R10 and reads said word via Port A(PA).
+        #10;
+        C = 4'b1010;
+        PW = 32'd16;
+        #10
+        SA = 4'b1010;
+        //Showing output through PA, after changing the word in Register 10
+        //$monitor ("Output of Register ", SA, " (using PA) (After Change): PA: %0d",PA);
+    $finish;
+    end
+
+endmodule
